f28054616e5effadeb2bc7eedcaf4bb0b1722733
Mofeyifoluwa David Akomolafe <mofeyifoluwa.akomolafe@mths.ca>
2023-01-12 22:24:57 +0000

    done

---
+++ array.cpp
@@ -0,0 +1,31 @@
+// Copyright (c) 2022 Feyi Akomolafe All rights reserved
+
+// Created by: Feyi Akomolafe
+// Created on: Dec 2022
+// This program finds the average of 10 random numbers using arrays
+
+#include <iostream>
+#include <random>
+
+int main() {
+    int randomNumbers[10];
+    int singleRandomNumber;
+    float average = 0;
+
+    // input
+    for (int loop_counter = 0; loop_counter < 10; loop_counter++) {
+        std::random_device rseed;
+        std::mt19937 rgen(rseed());
+        std::uniform_int_distribution<int> idist(0, 100);
+        singleRandomNumber = idist(rgen);
+        randomNumbers[loop_counter] = singleRandomNumber;
+        average = average + singleRandomNumber;
+        std::cout << "The random number is: "
+                  << randomNumbers[loop_counter] << "" << std::endl;
+    }
+    // adds all the numbers in the array
+    average = average / (sizeof(randomNumbers) / sizeof(randomNumbers[0]));
+    std::cout << "\nThe average is " << average << "" << std::endl;
+
+    std::cout << "\n.Done" << std::endl;
+}
